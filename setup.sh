1161019e618f015425efb0519a056e7b92896994
Turiiya <34311583+ttytm@users.noreply.github.com>
2024-05-30 19:59:43 +0200

    build: update setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-
-# Download helper for WebUI wrapper users to simplify the setup with the latest
-# WebUI-C versions - Go Prototype.
-#
-# Source: https://github.com/webui-dev/go-webui
-# License: MIT
+#!/usr/bin/env bash
 
 # Determine the release archive for the used platform and architecture.
 platform=$(uname -s)
@@ -16,7 +10,7 @@ case "$platform" in
 				archive="webui-linux-gcc-x64.zip"
 				;;
 			aarch64|arm64)
-				archive="webui-linux-gcc-aarch64.zip"
+				archive="webui-linux-gcc-arm64.zip"
 				;;
 			arm*)
 				archive="webui-linux-gcc-arm.zip"
@@ -50,22 +44,22 @@ esac
 # Parse CLI arguments.
 # Defaults.
 output="$(dirname  $0)/webui"
-nightly=true # TODO: After WebUI v2.4.0 release, remove default, to set nightly to false.
-while [[ $# -gt 0 ]]; do
+version="latest"
+while [ $# -gt 0 ]; do
 	case "$1" in
 		-o|--output)
 			output="$2"
 			shift
 			;;
 		--nightly)
-			nightly=true
+			version="nightly"
 			shift
 			;;
 		-h|--help)
 			echo -e "Usage: setup.sh [flags]\n"
 			echo "Flags:"
 			echo "  -o, --output: Specify the output directory"
-			echo "  --nightly: Download the latest nightly release"
+			echo "  --nightly: Download the development release"
 			echo "  -h, --help: Display this help message"
 			exit 0
 			;;
@@ -82,18 +76,18 @@ done
 # 	"$output/libwebui-2-static.a" "$output/webui-2.dylib" "$output/webui-2.dll"
 rm -rf "$output"
 
-# Download and extract the archive.
-echo "Downloading..."
 release_base_url="https://github.com/webui-dev/webui/releases/"
-if [ "$nightly" = true ]; then
+if [ "$version" = "nightly" ]; then
 	url="$release_base_url/download/nightly/$archive"
-else
+elif [ "$version" = "latest" ]; then
 	url="$release_base_url/latest/download/$archive"
 fi
+
+# Download and extract the archive.
+echo "Downloading WebUI@$version..."
 curl -L "$url" -o "$archive"
 echo ""
 
-# Move the extracted files to the output directory.
 echo "Extracting..."
 archive_dir="${archive%.zip}"
 unzip "$archive"
